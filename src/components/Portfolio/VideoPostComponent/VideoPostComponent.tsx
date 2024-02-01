7a87a232e92f9559667f25cf9e5bb79510a622ce
[GIT] БАЙКИ_ИНСАЙДЕРА <aleksandrov27@mail.ru>
2024-02-01 13:06:44 +0300

    initial

---
+++ src/components/Portfolio/VideoPostComponent/VideoPostComponent.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./VideoPostComponent.module.css";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+import { Routes } from "../../../router/router";
+
+interface IProps {
+  id: number;
+  title: string;
+  shortDescription: string;
+  preview: string;
+  tags: string[];
+  alias: string;
+}
+
+export const VideoPostComponent: React.FC<IProps> = (props) => {
+  const { id, title, shortDescription, preview, tags, alias } = props;
+
+  return (
+    <Link to={`${Routes.MAIN}details-post/${alias}`}>
+      <div key={id} className={styles.videoPost}>
+        <div className={styles.videoPreview}>
+          <img src={preview} alt={"previewPost"} />
+          <div className={styles.videoTags}>
+            {tags.map((tag, index) => {
+              return (
+                <div
+                  key={index}
+                  className={classNames(
+                    styles.videoTag,
+                    `${tag === "C#" ? tag.replace("#", "sharp") : tag}_tag`
+                  )}
+                >
+                  {tag}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <h3 className={styles.videoTitle}>«{title}»</h3>
+        <p className={styles.videoShortDescription}>{shortDescription}</p>
+      </div>
+    </Link>
+  );
+};
